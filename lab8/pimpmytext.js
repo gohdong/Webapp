ba94dd368b591d39883399648b90a9606056a7d0
aldehf420 <aldehf420@naver.com>
2019-11-28 19:50:46 +0900

    lab08

---
+++ lab8/pimpmytext.js
@@ -0,0 +1,37 @@
+"use strict";
+
+function buttonClick() {
+    var size = 12;
+    setInterval(function() {
+        size += 2;
+        var textbox = document.getElementById("test");
+        textbox.style.fontSize = `${size}pt`
+    }, 500);
+
+
+}
+
+function checkBling() {
+    // document.getElementById("myCheck").checked = true;
+    var checkbox = document.getElementById("check").checked;
+    if (checkbox == true) {
+        document.getElementById("test").style.fontWeight = "bold";
+        document.getElementById("test").style.textDecoration = "underline";
+        document.getElementById("test").style.color = "green";
+    } else {
+        document.getElementById("test").style.fontWeight = "normal";
+        document.getElementById("test").style.textDecoration = "none";
+        document.getElementById("test").style.color = "black";
+    }
+
+}
+
+function snoopify() {
+    var textbox = document.getElementById("test");
+    var partBox = textbox.value.split('.')
+    var jbStr2 = partBox.join('-izzle.');
+
+    // alert(textbox.value.split('.')[1])
+    textbox.value = jbStr2.toUpperCase();
+
+}
